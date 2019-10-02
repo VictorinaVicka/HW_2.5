88eed45e0767348b2d392a518a05700c01516dcd
VictorinaVicka <city_br@mail.ru>
2019-10-02 12:17:14 +0300

    Initial Commit

---
+++ HW_2.5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_2.5
+//
+//  Created by Виктория Воробьева on 02/10/2019.
+//  Copyright © 2019 Виктория Воробьева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
